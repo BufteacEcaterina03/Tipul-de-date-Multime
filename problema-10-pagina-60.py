f75eea563a36a7147a21798c806e0984306e60a4
BufteacEcaterina03 <70895514+BufteacEcaterina03@users.noreply.github.com>
2021-11-16 20:54:18 +0200

    Add files via upload

---
+++ problema-10-pagina-60.py
@@ -0,0 +1,14 @@
+Z={1,2,3,4}
+from itertools import combinations
+M=combinations (Z, 1)
+N=combinations (Z, 2)
+O=combinations (Z, 3)
+P=combinations (Z, 4)
+set1=set(M)
+print(set1)
+set2=set(N)
+print(set2)
+set3=set(O)
+print(set3)
+set4=set(P)
+print(set4)
---
+++ problema-11-pagina-60.py
@@ -0,0 +1,14 @@
+Y={'A','B','C','D'}
+from itertools import combinations
+F=combinations (Y, 1)
+G=combinations (Y, 2)
+H=combinations (Y, 3)
+J=combinations (Y, 4)
+set1=set(F)
+print(set1)
+set2=set(G)
+print(set2)
+set3=set(H)
+print(set3)
+set4=set(J)
+print(set4)
