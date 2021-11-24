223a4f45aea05b884a7ec1be6555a4a0de34485d
BufteacEcaterina03 <70895514+BufteacEcaterina03@users.noreply.github.com>
2021-11-24 20:18:30 +0200

    Delete problema-14-pagina-61.py

--- problema-14-pagina-61.py
+++
@@ -1,5 +0,0 @@
-z = set(input("Introduceti sirul  de caractere z: "))
-k = set(input("introdu sirul b de caractere: "))
-print("Caracterele care se  intalnesc cel putin in unul din sirui :", z.intersection(k))
-print(" Caracterele care apar  in ambele siruri: ", z.union(k))
-print("Caracterele care apar in primul si nu apar in sirul al doilea: ", z.difference(k))
